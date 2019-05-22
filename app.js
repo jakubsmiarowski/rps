38038fea647c4ebe419dfc97786ea3fba0f115e4
Jakub Smiarowski <jakub.smiarowski@gmail.com>
2019-05-22 12:04:58 +0200

    Modal feature

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 //variables
 var params = {
-    rock: 'Rock',
-    paper: 'Paper',
-    scissors: 'Scissors',
+    rock: "Rock",
+    paper: "Paper",
+    scissors: "Scissors",
     userMove: "",
     compMove: "",
     rounds: 0,
     userScore: 0,
     compScore: 0,
-    progress: [],
+    progress: []
 };
 
-var overlay = document.getElementById('overlay');
-var modal = document.getElementById('modal');
+var overlay = document.getElementById("overlay");
+var modal = document.getElementById("modal");
 var modalHeader = document.querySelector(".modal > h1");
 
 //outputs
 var userScoreBoard = document.getElementById("user-score");
 var compScoreBoard = document.getElementById("computer-score");
 var output = document.querySelector(".output > p");
-var actionMessage = document.querySelector(".action-message")
+var actionMessage = document.querySelector(".action-message");
+var table = document.getElementById("modalTable");
 
 //buttons variables
-var pickRock = document.getElementById('rock');
-var pickPaper = document.getElementById('paper');
-var pickScissors = document.getElementById('scissors');
+var pickRock = document.getElementById("rock");
+var pickPaper = document.getElementById("paper");
+var pickScissors = document.getElementById("scissors");
 var newGame = document.querySelector(".new-game");
-
 
 //functions
 
@@ -42,16 +42,30 @@ function compChoice() {
 }
 
 function compare() {
-    compMove = compChoice();
-    if ((params.userMove == params.paper && params.compMove == params.rock) || (params.userMove == params.rock && params.compMove == params.scissors) || (params.userMove == params.scissors && params.compMove == params.paper)) {
-        output.innerHTML = params.userMove + " beats " + params.compMove + ". You won!";
-        userScoreBoard.innerHTML = 1 + params.userScore++;
+    params.compMove = compChoice();
+    console.log(params.compMove);
+    console.log(params.userMove);
+    if (
+        (params.userMove == params.paper && params.compMove == params.rock) ||
+        (params.userMove == params.rock && params.compMove == params.scissors) ||
+        (params.userMove == params.scissors && params.compMove == params.paper)
+    ) {
+        output.innerHTML =
+            params.userMove + " beats " + params.compMove + ". You won!";
+        params.userScore++;
+        userScoreBoard.innerHTML = params.userScore;
         checkWinner();
         update();
         table();
-    } else if ((params.userMove == params.rock && params.compMove == params.paper) || (params.userMove == params.scissors && params.compMove == params.rock) || (params.userMove == params.paper && params.compMove == params.scissors)) {
-        output.innerHTML = params.compMove + " beats " + params.userMove + ". You lose!";
-        compScoreBoard.innerHTML = 1 + params.compScore++;
+    } else if (
+        (params.userMove == params.rock && params.compMove == params.paper) ||
+        (params.userMove == params.scissors && params.compMove == params.rock) ||
+        (params.userMove == params.paper && params.compMove == params.scissors)
+    ) {
+        params.compScore++;
+        output.innerHTML =
+            params.compMove + " beats " + params.userMove + ". You lose!";
+        compScoreBoard.innerHTML = params.compScore;
         checkWinner();
         update();
         table();
@@ -64,11 +78,12 @@ function compare() {
 }
 
 function checkWinner() {
-
     if (params.userScore == params.rounds) {
-        rounds = alert('You won entire game! Congratulations!');
+        table.innerHTML = "You won entire game! Congratulations!";
+        showModal();
     } else if (params.compScore == params.rounds) {
-        rounds = alert('You lost entire game :( Better luck next time!');
+        table.innerHTML = "You lost entire game :( Better luck next time!";
+        showModal();
     }
 }
 
@@ -77,27 +92,46 @@ function update() {
         roundNumber: params.rounds,
         userPick: params.userMove,
         compPick: params.compMove,
-        userWon: params.userScore++,
-        compWon: params.compScore++,
-    }
+        userWon: params.userScore,
+        compWon: params.compScore
+    };
     params.progress.push(object);
 }
 
 function table() {
-    var table = document.getElementById("modalTable");
+
+    table.innerHTML = "";
     for (var i = 0; i < params.progress.length; i++) {
-        table.innerHTML = "<tr> + <td> + params.progress[i].roundNumber + </td> <br>"
+        table.innerHTML +=
+            "<tr><td>" +
+            i +
+            "</td><td>" +
+            params.progress[i].userPick +
+            "</td></tr>";
+        table.innerHTML +=
+            "<tr><td>" +
+            i +
+            "</td><td>" +
+            params.progress[i].userWon +
+            "</td></tr>";
+        table.innerHTML +=
+            "<tr><td>" +
+            i +
+            "</td><td>" +
+            params.progress[i].compWon +
+            "</td></tr>";
     }
 }
 
+
 function showModal() {
     event.preventDefault();
-    overlay.classList.add('show');
+    overlay.classList.add("show");
 }
 
 function hideModal() {
     event.preventDefault();
-    overlay.classList.remove('show');
+    overlay.classList.remove("show");
 }
 
 function reset() {
@@ -106,49 +140,47 @@ function reset() {
     params.compScore = 0;
     userScoreBoard.innerHTML = 0;
     compScoreBoard.innerHTML = 0;
-    output.innerHTML = ('Try your luck!');
+    output.innerHTML = "Try your luck!";
 }
 
 function endGame() {
-    params.rounds = window.prompt('How many rounds did you win?');
-    showModal();
+    params.rounds = window.prompt("How many rounds are you playing?");
     if (isNaN(params.rounds)) {
-        alert('Please pick a number!');
+        alert("Please pick a number!");
         endGame();
     }
 }
 
-
-
 //buttons listeners
 
-pickRock.addEventListener('click', function() {
-    params.userMove == params.rock;
+pickRock.addEventListener("click", function() {
+    params.userMove = params.rock;
     compare();
 });
 
-pickPaper.addEventListener('click', function() {
-    params.userMove == params.paper;
+pickPaper.addEventListener("click", function() {
+    console.log("s");
+    params.userMove = params.paper;
     compare();
 });
 
-pickScissors.addEventListener('click', function() {
-    params.userMove == params.scissors;
+pickScissors.addEventListener("click", function() {
+    params.userMove = params.scissors;
     compare();
 });
 
 //closing modal
 
-overlay.addEventListener('click', function(e) {
+overlay.addEventListener("click", function(e) {
     if (e.target === this) {
-        closeModal();
+        hideModal();
     }
 });
 
 
 // NEWGAME
 
-newGame.addEventListener('click', function() {
+newGame.addEventListener("click", function() {
     reset();
     endGame();
 });
